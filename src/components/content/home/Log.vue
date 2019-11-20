a98ab67a5b22b3bab5c9c36684f5e3992a4b9ea5
a888888hhb <a888888hhb@outlook.com>
2019-11-20 17:15:15 +0800

    架构基本确定

--- src/components/content/home/Log.vue
+++ src/components/content/home/Log.vue
@@ -1,23 +1,45 @@
 <template>
   <div class="content markdown-body">
-    <markdown />
+     <component v-bind:is="componentFile"></component>
   </div>
 </template>
 
 <script>
 import "highlight.js/styles/github.css";
-import 'github-markdown-css'
-import markdown from "./../../../assets/blogMd/prototype.md";
+import "github-markdown-css";
 import blogs from "./../../../assets/blog.json";
 export default {
+  created() {},
   data() {
+    const componentFile = this.render;
     return {
-      blogs
-    };
+      blogs,
+      componentFile,
+    }
   },
-  components: {
-    markdown
+  computed: {
+    log() {
+      return this.blogs.find(item => {
+        return item.id === this.$route.params.id;
+      });
+    }
   },
+    methods: {
+      render () {
+        this.componentFile =  () => ({
+          component: import(`./../../../assets/blogMd/${this.log?this.log.filename:"this.md"}`),
+          loading: { template: '<div style="height: 100%; width: 100%; display: table;"><div style="display: table-cell; vertical-align: middle; text-align: center;"><div>加载中</div></div></div>' },
+          error:  { template: '<div style="height: 100%; width: 100%; display: table;"><div style="display: table-cell; vertical-align: middle; text-align: center;"><div>加载错误</div></div></div>' },
+          delay: 200,
+          timeout: 10000
+        });
+      }
+    },
+    watch: {
+      log () {
+        this.render()
+      }
+    }
 };
 </script>
 
@@ -29,5 +51,6 @@ export default {
   border: 1px solid #d1d5da;
   border-radius: 3px;
   padding: 1rem;
+  font-size: 1.2rem;
 }
 </style>
--- src/components/content/home/LogList.vue
+++ src/components/content/home/LogList.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="content">
-    <div class="title">{{ log.title }}</div>
+    <h3 class="title">{{ log.title }}</h3>
     <div class="logInfo">
-      <span>发表于{{log.updatedAt}}</span>
-      <span>字数统计</span>
-      <span>标签{{log.layout}}</span>
+      <span>发表于：{{log.updatedAt}}</span>
+      <span>字数统计：{{log.wordcount}}</span>
+      <span>标签：{{log.tag}}</span>
     </div>
   </div>
 </template>
@@ -13,35 +13,31 @@
 
 export default {
   props: { log: Object },
-  created(){
-    console.log(this.params);
-  },
   data() {
     return {
       params: this.log,
     };
-  },
-  components: {
-    // Log
   }
 };
 </script>
 
 <style scoped>
 .content {
-  background-color: orange;
+  background-color: fff;
   display: flex;
   flex-direction: column;
-  width: 85%;
-  height: 20rem;
+  width: 80%;
   margin: 2rem auto;
   box-shadow: 0px 0px 10px #ccc;
   border-radius: 0.5rem;
-  padding: 2rem 1rem;
+  padding: 1rem;
 }
 .logInfo {
   margin: 1rem 0;
   display: flex;
-  justify-content: space-between;
+  justify-content: space-evenly;
+}
+.title{
+  text-align: center;
 }
 </style>
