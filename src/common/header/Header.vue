c2d169bd5a2d701a4335d2a9c809032825fd98ac
khb <a888888hhb@outlook.com>
2019-12-16 22:06:52 +0800

    add content-xs

--- src/common/header/Header.vue
+++ src/common/header/Header.vue
@@ -2,7 +2,7 @@
   <div class="header">
     <div class="header-content">
       <div class="header-logo">
-        <router-link tag="span" to="/">Shiroi</router-link>
+        <router-link tag="span" to="/">Shiro</router-link>
       </div>
       <div @click="handleMenuClick">
         <icon-svg class="menu" icon-class="menu"></icon-svg>
@@ -22,14 +22,45 @@
               class="categories-icon"
               icon-class="sort-down"
             ></icon-svg>
+            <div v-if="nav === 'Categories'" class="categories-">
+
+            </div>
           </div>
         </div>
         <div class="header-input">
           <input type="text" v-model="keyWord" />
-          <icon-svg class="search-icon" icon-class="search"></icon-svg>
+          <div class="search-icon">
+            <icon-svg icon-class="search"></icon-svg>
+          </div>
         </div>
       </div>
     </div>
+   <transition name="fade">
+      <div class="content-container-xs" v-if="showContainer">
+        <div class="content-list-xs">
+          <div
+            @click="handleItemClick"
+            class="header-item-xs"
+            v-for="(nav, index) in navList"
+            :key="index"
+          >
+            <icon-svg class="header-icon-xs" :icon-class="nav"></icon-svg>
+            <span class="item-desc-xs">{{ nav }}</span>
+            <icon-svg
+              v-if="nav === 'Categories'"
+              class="categories-icon-xs"
+              icon-class="sort-down"
+            ></icon-svg>
+          </div>
+        </div>
+        <div class="header-input-xs">
+          <input type="text" v-model="keyWord" />
+          <div class="search-icon-xs">
+            <icon-svg icon-class="search"></icon-svg>
+          </div>
+        </div>
+      </div>
+   </transition>
   </div>
 </template>
 
@@ -40,16 +71,27 @@ export default {
     return {
       keyWord: "Search",
       navList: ["Categories", "Tags", "Archive", "About"],
-      cclientWidth: 0
+      clientWidth: 0,
+      showContainer:false
     };
   },
   mounted() {
     this.clientWidth = `${document.documentElement.clientWidth}px`;
-    // 监听window的resize事件
-    const that = this;
-    window.onresize = this.throttle(function temp() {
-      that.clientWidth = `${document.documentElement.clientWidth}px`;
-    }, 16);
+    // 监听 window 的 resize 事件
+    window.onresize = this.throttle(()=>{
+      this.clientWidth = `${document.documentElement.clientWidth}px`;
+      // 隐藏 .content-container-xs 的内容
+      if(parseInt(this.clientWidth) > 963){
+        this.showContainer = false
+      }
+    }, 50);
+  },
+  watch: {
+    // clientWidth(){
+    //   if(parseInt(this.clientWidth) > 980){
+    //     this.showContainer = false
+    //   }
+    // }
   },
   components: {
     IconSvg
@@ -74,7 +116,7 @@ export default {
       console.log(e.target);
     },
     handleMenuClick() {
-      console.log(this.clientWidth);
+      this.showContainer = !this.showContainer
     }
   }
 };
@@ -91,7 +133,7 @@ export default {
       width 90% !important
   @media (max-width : 980px)
     .header-content
-      width 70% !important
+      width 80% !important
       display flex
       justify-content space-between
       .content-container
@@ -138,23 +180,68 @@ export default {
           .categories-icon
             font-size .34rem
             padding-left .2rem
-    .header-input
-      width 22%
-      background-color rgba(255, 255, 255, 0.2)
-      border-radius .1rem
-      box-sizing border-box
-      display flex
-      align-items center
-      input
-        background-color rgba(255, 255, 255, 0)
-        color: #fff
-        height 100%
-        width 90%
+      .header-input
+        width 22%
+        background-color rgba(255, 255, 255, 0.2)
+        border-radius .1rem
         box-sizing border-box
-        padding-left .2rem
-        font-size .40rem
-      .search-icon
-        font-size .40rem
-        color #fff
-        padding 0 .2rem
+        display flex
+        align-items center
+        justify-content space-between
+        input
+          background-color rgba(255, 255, 255, 0)
+          color: #fff
+          height 100%
+          width 90%
+          box-sizing border-box
+          padding-left .2rem
+          font-size .40rem
+        .search-icon
+          font-size .40rem
+          color #fff
+          padding 0 .2rem
+  .fade-enter-active, 
+  .fade-leave-active 
+    transition opacity  0.5s cubic-bezier(0.04, 0.04, 0.12, 0.96)
+  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ 
+    opacity 0
+  .content-container-xs
+    width 80%
+    display flex
+    flex-direction column
+    margin 0 auto
+    .content-list-xs
+        .header-item-xs
+          cursor: pointer
+          font-size .40rem
+          color rgba(255, 255, 255, 0.7)
+          &:hover
+            color #fff
+          .item-desc-xs
+            padding-left .1rem
+          .categories-icon-xs
+            font-size .34rem
+            padding-left .2rem
+      .header-input-xs
+        margin-bottom .2rem
+        width 100%
+        height .86rem
+        background-color rgba(255, 255, 255, 0.2)
+        border-radius .1rem
+        box-sizing border-box
+        display flex
+        align-items center
+        justify-content space-between
+        input
+          background-color rgba(255, 255, 255, 0)
+          color: #fff
+          height 100%
+          width 90%
+          box-sizing border-box
+          padding-left .2rem
+          font-size .40rem
+        .search-icon-xs
+          font-size .40rem
+          color #fff
+          padding 0 .2rem
 </style>
